7cb13742f1f7383533d7de08eeab9b5daad52d5c
Anna Akopyan <anchello@yandex.ru>
2021-05-18 12:46:24 +0300

    Add project files.

---
+++ project1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+// This function prints
+
+void print()
+{
+    // Вызываем оператор << на объекте типа std::cout
+    std::cout << "Hello Skillbox!\n";
+
+}
+
+/*
+* Приписываем значение переменным и печатаем Hello World
+*/
+
+int main()
+{
+    int x = 100;
+    3 + 7;
+    int y = x + 100;
+
+    int b = 0;
+    b = b + 2;
+
+    int test;
+    test = 100500;
+
+    int test2 = 1005001;
+
+    int mult = x * y;
+
+    int rundom = 1002;
+
+    std::cout << "Hello World!\n";
+
+  
+}
